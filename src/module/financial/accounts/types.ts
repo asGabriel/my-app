e87adb9da85e95dc08528699d9885d9651334eaa
Gabriel Andrade <andrade.gabriel2295@gmail.com>
2026-01-17 09:30:13 -0300

    refac: reset project

--- src/module/financial/accounts/types.ts
+++
@@ -1,5 +0,0 @@
-export interface Account {
-    accountId: string,
-    bankName: string,
-    owner: string
-}
--- src/module/financial/invoices/types.ts
+++
@@ -1,8 +0,0 @@
-export interface Invoice {
-    invoiceId: string,
-    title: string,
-    month: number,
-    year: number,
-    isMain: boolean,
-    createdAt: string
-}
