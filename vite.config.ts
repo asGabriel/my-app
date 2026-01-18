b0780cc176a0ef6cf5b74b9cb2dd901c93aef465
Gabriel Andrade <andrade.gabriel2295@gmail.com>
2026-01-17 22:02:29 -0300

    feat: cors vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://rust-api-h0m0.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
